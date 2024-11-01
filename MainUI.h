5e883904ff9669940f63f35593cd8709956d7545
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-11-01 19:42:38 +0200

    Create MainUI.h

---
+++ MainUI.h
@@ -0,0 +1,21 @@
+#ifndef MAINUI_H
+#define MAINUI_H
+
+#include "Machine.h"
+#include <string>
+
+class MainUI {
+private:
+    Machine machine;
+    bool enterFileOrInstructions;
+
+public:
+    MainUI();
+    void displayMenu();
+    void inputFileName();
+    void inputInstruction();
+    void runProgram();
+    char inputChoice();
+};
+
+#endif
