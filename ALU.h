23856f84c8ecb20d6781c58c2a2a9eaf7489c495
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-11-03 23:40:07 +0200

    Update ALU.h

--- ALU.h
+++ ALU.h
@@ -4,19 +4,22 @@
 #include "Register.h"
 #include <string>
 
+using namespace std;
+
 class ALU {
 public:
-    // Add two's complement values from two registers and store the result in the destination register
-    void add(int regR, int regS, int regT, Register& reg);
-
-    // Add floating-point values from two registers and store the result in the destination register
-    void addFloatingPoint(int regR, int regS, int regT, Register& reg);
-
-    // Helper functions for hexadecimal to decimal conversion and vice versa
     int hexToDec(const std::string& hex);
     std::string decToHex(int dec);
 
     static bool isValid(const std::string& hexStr);
+    // Add two's complement values from two registers and store the result in the destination register
+    void add(int regR, int regS, int regT, Register& reg);
+
+    // Add floating-pstd::stringalues from two registers and store the result in the destination register
+    void addFloatingPoint(int idx1, int idx2, int resultIdx, Register& reg);
+
+    // Helper functions for hexadecimal to decimal conversion and vice versa
+
 };
 
 #endif
