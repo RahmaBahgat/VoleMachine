bd28b0c9830c6469977f1fe45ed9183599d49b98
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-11-06 22:08:56 +0200

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -10,8 +10,8 @@ class Machine {
 private:
     CPU processor;
     Memory memory;
-    bool isASCII(const std::string& str) const; 
-    bool isHex(const std::string& str) const;   
+    bool isASCII(const string& str) const;
+    bool isHex(const string& str) const;
 
 public:
     Machine();
