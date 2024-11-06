fb11a2d1318e695c03def6b708a9a94300d95dee
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-11-06 22:05:01 +0200

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -10,12 +10,15 @@ class Machine {
 private:
     CPU processor;
     Memory memory;
+    bool isASCII(const std::string& str) const; 
+    bool isHex(const std::string& str) const;   
 
 public:
     Machine();
     void loadProgramFile(const string& program);
     void outputState() const;
     int executeNextInstruction();
+
 
 };
 
