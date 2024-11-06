1b492f193f9acd60fa5d8b3406f3e54564efc192
rawdaraafat <144979504+rawdaraafat@users.noreply.github.com>
2024-11-06 20:40:43 +0200

    Update Register.cpp

--- Register.cpp
+++ Register.cpp
@@ -1,11 +1,12 @@
+
 #include "Register.h"
 
 Register::Register(int size) : size(size), memory(size, "00") {}
 
-std::string Register::getCell(int address) const {
+string Register::getCell(int address) const {
     return memory[address];
 }
 
-void Register::setCell(int address, const std::string& val) {
+void Register::setCell(int address, const string& val) {
     memory[address] = val;
 }
